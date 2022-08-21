10c507e3cf5730998cd463b09c052ca278c55bf0
jayjaychukwu <liljaydon.pj@gmail.com>
2022-08-21 23:27:46 +0100

    Working with static files for the django app

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = ((os.path.join(BASE_DIR, "static")),)
+STATIC_ROOT = str((os.path.join(BASE_DIR, "staticfiles")))
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+# STATICFILES_DIR = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
