67b337424653345b2a48170fa076a31133d71c96
jayjaychukwu <liljaydon.pj@gmail.com>
2022-09-05 17:01:30 +0100

    Added environment variables with environs package

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,5 +1,11 @@
-from pathlib import Path
 import os
+from email.policy import default
+from pathlib import Path
+
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +15,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vij@yp6+k-2fen55nfyk9o8x2gpnw&_732hvrnfgf+-dhy=^nx"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -75,7 +81,7 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": env.dj_db_url("DATABASE_URL", default="postgres://postgres@db/postgres"),
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
